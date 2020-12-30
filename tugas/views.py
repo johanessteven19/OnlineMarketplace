0082254733e747d7c11d8ad99fc2e6a130414312
hazlazuardi <hazlazuardi@gmail.com>
2020-12-30 21:16:42 +0700

    register visitor and organizer

--- tugas/views.py
+++ tugas/views.py
@@ -14,10 +14,44 @@ def editprofile(request):
     return render(request,'pages/editprofile.html',{})
 
 def registervisitor(request):
-    return render(request,'pages/registervisitor.html',{})
+    if(request.method=="POST"):
+        profile = {}
+        data= request.POST
+        email = data['your_email']
+        password = data['your_password']
+        fname = data['your_fname']
+        lname = data['your_lname']
+        cursor = connection.cursor()
+        cursor.execute("SET search_path TO EVENT;")
+        cursor.execute("INSERT INTO EVENT.USER VALUES (%(email)s, %(password)s);",
+                                {'email': email, 'password': password})
+        cursor.execute("INSERT INTO EVENT.VISITOR VALUES (%(email)s, %(fname)s, %(lname)s);", {'email': email, 'fname': fname, 'lname': lname})
+        profile['first_name']=fname
+        profile['last_name'] = lname
+        return render(request,'pages/index.html',{'profile':[profile]})
+
+    else:
+        return render(request,'pages/registervisitor.html',{})
     
 def registerorganizer(request):
-    return render(request,'pages/registerorganizer.html',{})
+    if(request.method=="POST"):
+        profile = {}
+        data= request.POST
+        email = data['your_email']
+        password = data['your_password']
+        fname = data['your_fname']
+        lname = data['your_lname']
+        npwp = data['your_npwp_ktp']
+        cursor = connection.cursor()
+        cursor.execute("SET search_path TO EVENT;")
+        cursor.execute("INSERT INTO EVENT.USER VALUES (%(email)s, %(password)s);",
+                                {'email': email, 'password': password})
+        cursor.execute("INSERT INTO EVENT.ORGANIZER VALUES (%(email)s, %(npwp)s);", {'email': email, 'npwp': npwp})
+        profile['first_name']=fname
+        profile['last_name'] = lname
+        return render(request,'pages/index.html',{'profile':[profile]})
+    else:
+        return render(request,'pages/registerorganizer.html',{})
 
 def ewallet(request):
     return render(request,'pages/ewallet.html',{})
@@ -120,3 +154,9 @@ def loginVisitor(request):
             return render(request,'registration/loginVisitor.html',{'message':message})
     else:
         return render(request,'registration/loginVisitor.html',{})
+
+# host : ec2-54-175-243-75.compute-1.amazonaws.com
+# database: d40bv1a9pdpmo0
+# user : msoobvwvovwzyi
+# port : 5432
+# password : 2cb80209623cb2d61652ba09fd10d1d011a7edb48ae10bc1a55b3dd8d9b184c2
